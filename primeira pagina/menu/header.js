8641e9cbb97d18234ed76e09946ebc7ba63c9be7
Kirigas <100741364+PedroHenriqueVBS@users.noreply.github.com>
2023-06-01 13:56:30 -0300

    hamburguer navbar

--- primeira pagina/menu/header.js
+++ primeira pagina/menu/header.js
@@ -1,7 +1,43 @@
 const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu() {
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault()
     const nav = document.getElementById('nav')
     nav.classList.toggle('active')
 }
+
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+
+
+
+
+
+
+/*class MobileNavbar {
+    constructor ( mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+    }
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", () => console.log("okokok"));
+    }
+
+    Init() {
+        if(this.mobileMenu) {
+            this.addClickEvent();
+        }
+         return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar (
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+mobileNavbar.Init()*/
