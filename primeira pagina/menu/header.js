7df1c93605dee6d0d8a4054459d4904718610abe
Kirigas <100741364+PedroHenriqueVBS@users.noreply.github.com>
2023-06-02 16:29:14 -0300

    header finalizado

--- primeira pagina/menu/header.js
+++ primeira pagina/menu/header.js
@@ -16,28 +16,3 @@ btnMobile.addEventListener('touchstart', toggleMenu);
 
 
 
-/*class MobileNavbar {
-    constructor ( mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
-    }
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", () => console.log("okokok"));
-    }
-
-    Init() {
-        if(this.mobileMenu) {
-            this.addClickEvent();
-        }
-         return this;
-    }
-}
-
-const mobileNavbar = new MobileNavbar (
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-);
-mobileNavbar.Init()*/
