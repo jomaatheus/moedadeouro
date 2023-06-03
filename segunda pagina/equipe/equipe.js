a3133075f9d566237b385dd2e443daa576fc77c0
João Matheus <117495280+jomaatheus@users.noreply.github.com>
2023-06-02 21:16:24 -0300

    Create equipe.js

---
+++ segunda pagina/equipe/equipe.js
@@ -0,0 +1,7 @@
+function trocarImagem(elemento) {
+  elemento.src = "../../imagens/imagem-equipe/alanSocial.jpg"; /* Altera o caminho da imagem quando o mouse passa por cima */
+}
+
+function restaurarImagem(elemento) {
+  elemento.src = "../../imagens/imagem-equipe/alanTerno.jpg"; /* Retorna à imagem original quando o mouse sai de cima */
+}
